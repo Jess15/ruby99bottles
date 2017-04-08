6451e82d15caaaae7e3cebc06c8e092bde64b24d
Jesica Espejo <krash@Krash.local>
2017-04-08 03:20:53 -0300

    test_a_couple_verses [test 3]

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,34 +5,30 @@ class Bottles
 	end
 
 	def verse(n)
-		@vrs = <<-VERSE
-#{n} bottles of beer on the wall, #{n} bottles of beer.
-Take one down and pass it around, #{n-1} bottles of beer on the wall.
-VERSE
-		self.substitution(n, @vrs)
+		self.substitution(n)
 	end
 
 	def verses(x, y)
+		couple = substitution(x)+"\n"+"\n"+substitution(y)
+		return couple 
+	end
+
+	def substitution(n)
 		@vrs = <<-VERSE
 #{n} bottles of beer on the wall, #{n} bottles of beer.
 Take one down and pass it around, #{n-1} bottles of beer on the wall.
 VERSE
-		couple = substitution(x, @vrs)+"\n\n"+substitution(y, @vrs)
-		return couple 
-	end
-
-	def substitution(n, vrs)
 		if n==2
-	  	vrs = vrs.sub("bottles of beer on the wall.", "bottle of beer on the wall.")
+	  	@vrs = @vrs.sub("bottles of beer on the wall.", "bottle of beer on the wall.")
 	  elsif n==1
-	  	vrs = vrs.gsub("bottles", "bottle")
-	  	vrs = vrs.sub("one", "it")
-	  	vrs = vrs.sub("#{n-1} bottle of beer on the wall.", "no more bottles of beer on the wall.")
+	  	@vrs = @vrs.gsub("bottles", "bottle")
+	  	@vrs = @vrs.sub("one", "it")
+	  	@vrs = @vrs.sub("#{n-1} bottle of beer on the wall.", "no more bottles of beer on the wall.")
 	  elsif n==0
-	  	vrs = vrs.gsub("#{n} bottles of beer on", "No more bottles of beer on")
-	  	vrs = vrs.gsub("#{n} bottles of beer.", "no more bottles of beer.")
-	  	vrs = vrs.gsub("Take one down and pass it around,", "Go to the store and buy some more,")
-	  	vrs = vrs.gsub("#{n-1}", "#{n+99}")
+	  	@vrs = @vrs.gsub("#{n} bottles of beer on", "No more bottles of beer on")
+	  	@vrs = @vrs.gsub("#{n} bottles of beer.", "no more bottles of beer.")
+	  	@vrs = @vrs.gsub("Take one down and pass it around,", "Go to the store and buy some more,")
+	  	@vrs = @vrs.gsub("#{n-1}", "#{n+99}")
 	  end
 	  return vrs
 	end
