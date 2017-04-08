309294720be855be21bed67719e2944e63c8f738
Jesica Espejo <krash@Krash.local>
2017-04-08 02:22:52 -0300

    test_verse_2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,16 @@
 class Bottles
 	def verse(n)
-		result = <<-VERSE
+		vrs = <<-VERSE
 #{n} bottles of beer on the wall, #{n} bottles of beer.
 Take one down and pass it around, #{n-1} bottles of beer on the wall.
 VERSE
+		self.substitution(n, vrs)
+	end
+
+	def substitution(n, vrs)
+		if n==2
+	  	vrs = vrs.sub("bottles of beer on the wall.", "bottle of beer on the wall.")
+	  end
+	  return vrs
 	end
 end
