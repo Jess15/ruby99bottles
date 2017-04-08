7b9a3baac6361b417f031780ebabf4f6c81d5c47
Jesica Espejo <krash@Krash.local>
2017-04-08 03:13:14 -0300

    test_a_couple_verses [test 2]

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,10 +1,24 @@
 class Bottles
+
+	def initialize
+		@vrs = ""
+	end
+
 	def verse(n)
-		vrs = <<-VERSE
+		@vrs = <<-VERSE
 #{n} bottles of beer on the wall, #{n} bottles of beer.
 Take one down and pass it around, #{n-1} bottles of beer on the wall.
 VERSE
-		self.substitution(n, vrs)
+		self.substitution(n, @vrs)
+	end
+
+	def verses(x, y)
+		@vrs = <<-VERSE
+#{n} bottles of beer on the wall, #{n} bottles of beer.
+Take one down and pass it around, #{n-1} bottles of beer on the wall.
+VERSE
+		couple = substitution(x, @vrs)+"\n\n"+substitution(y, @vrs)
+		return couple 
 	end
 
 	def substitution(n, vrs)
